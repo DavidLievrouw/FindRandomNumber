9b1ed56187fb81bc14925be4f1adbc9dd4784e29
David Lievrouw <dlievrouw2@csc.com>
2016-07-04 16:31:28 +0200

    Add some functionality to the GuessingSequence.

--- src/FindRandomNumber.Guesser/GuessingSequence.cs
+++ src/FindRandomNumber.Guesser/GuessingSequence.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace FindRandomNumber.Guesser {
   public class GuessingSequence : IEnumerable<Guess> {
-    readonly IEnumerable<Guess> _guesses;
+    readonly List<Guess> _guesses;
 
     public GuessingSequence(IEnumerable<Guess> guesses) {
       if (guesses == null) throw new ArgumentNullException(nameof(guesses));
-      _guesses = guesses;
+      _guesses = guesses.ToList();
+      if (!_guesses.Any()) throw new ArgumentException("No guesses are specified.", nameof(guesses));
     }
 
     public IEnumerator<Guess> GetEnumerator() {
@@ -17,6 +19,22 @@ namespace FindRandomNumber.Guesser {
 
     IEnumerator IEnumerable.GetEnumerator() {
       return _guesses.GetEnumerator();
+    }
+
+    public GuessingSequenceSummary GetSummary() {
+      return new GuessingSequenceSummary(
+        _guesses.Count,
+        _guesses.Last().GuessedValue);
+    }
+
+    public class GuessingSequenceSummary {
+      public GuessingSequenceSummary(int numberOfAttempts, short guessedNumber) {
+        NumberOfAttempts = numberOfAttempts;
+        GuessedNumber = guessedNumber;
+      }
+
+      public int NumberOfAttempts { get; }
+      public short GuessedNumber { get; }
     }
   }
 }
---
+++ src/FindRandomNumber.Tests/Guesser/GuessingSequenceTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace FindRandomNumber.Guesser {
+  [TestFixture]
+  public class GuessingSequenceTests {
+    IEnumerable<Guess> _guesses;
+    GuessingSequence _sut;
+
+    [SetUp]
+    public void SetUp() {
+      _guesses = new[] { new Guess(1, false), new Guess(2, false), new Guess(3, false), new Guess(6, true) };
+      _sut = new GuessingSequence(_guesses);
+    }
+
+    [TestFixture]
+    public class Construction : GuessingSequenceTests {
+      [Test]
+      public void GivenNullGuesses_Throws() {
+        Assert.Throws<ArgumentNullException>(() => new GuessingSequence(null));
+      }
+
+      [Test]
+      public void GivenZeroGuesses_Throws() {
+        Assert.Throws<ArgumentException>(() => new GuessingSequence(new Guess[0]));
+      }
+    }
+
+    [TestFixture]
+    public class GetSummary : GuessingSequenceTests {
+      [Test]
+      public void BuildsGuessingSummaryWithExpectedValues() {
+        var actual = _sut.GetSummary();
+        Assert.That(actual, Is.Not.Null);
+        Assert.That(actual.GuessedNumber, Is.EqualTo(6));
+        Assert.That(actual.NumberOfAttempts, Is.EqualTo(4));
+      }
+    }
+  }
+}
