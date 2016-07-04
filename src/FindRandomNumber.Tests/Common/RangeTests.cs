5e5864586f1ec2610865de24ff989c27e07a2532
David Lievrouw <dlievrouw2@csc.com>
2016-07-04 22:29:23 +0200

    Add missing unit tests for Range equality.

--- src/FindRandomNumber.Tests/Common/RangeTests.cs
+++ src/FindRandomNumber.Tests/Common/RangeTests.cs
@@ -25,5 +25,42 @@ namespace FindRandomNumber.Common {
         Assert.That(actual.Maximum, Is.EqualTo(200));
       }
     }
+
+    [TestFixture]
+    public class Equality : RangeTests {
+      [Test]
+      public void EmptyInstances_AreEqual() {
+        var obj1 = new Range();
+        var obj2 = new Range();
+
+        Assert.That(obj1.Equals(obj2));
+        Assert.That(obj1.GetHashCode(), Is.EqualTo(obj2.GetHashCode()));
+        Assert.That(obj1 == obj2);
+      }
+
+      [Test]
+      public void ObjectsWithEqualValues_AreEqual() {
+        var obj1 = new Range(321, 456);
+        var obj2 = new Range(321, 456);
+
+        Assert.That(obj1.Equals(obj2));
+        Assert.That(obj1 == obj2);
+        Assert.That(obj1.GetHashCode(), Is.EqualTo(obj2.GetHashCode()));
+      }
+
+      [Test]
+      public void ObjectsWithDifferentValues_AreNotEqual() {
+        var obj1 = new Range(1, 123);
+        var obj2 = new Range(1, 124);
+        var obj3 = new Range(0, 123);
+
+        Assert.That(!obj1.Equals(obj2));
+        Assert.That(obj1 != obj2);
+        Assert.That(!obj2.Equals(obj3));
+        Assert.That(obj2 != obj3);
+        Assert.That(!obj1.Equals(obj3));
+        Assert.That(obj1 != obj3);
+      }
+    }
   }
 }
